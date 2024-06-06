fb833386cbf81782e16002b6110ea742680eacf7
chuttmateo <chuttmateo0@gmail.com>
2024-06-06 19:59:36 -0300

    last changes

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+function Page() {
+  return (
+    <div>Invoices page</div>
+  )
+}
+
+export default Page
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+//Primary font
+export const inter = Inter({ subsets: ['latin'] });
+//Secondary font
+export const lusitana = Lusitana({weight: '400', subsets: ['latin']});
