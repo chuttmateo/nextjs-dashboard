f8ec74f950f5a525f40af538fa2eb558d922e4d2
chuttmateo <chuttmateo0@gmail.com>
2024-06-12 19:16:45 -0300

    chapter 12

---
+++ app/dashboard/invoices/loading.tsx
@@ -0,0 +1,10 @@
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
+import React from 'react'
+
+function loading() {
+  return (
+    <InvoicesTableSkeleton />
+  )
+}
+
+export default loading
